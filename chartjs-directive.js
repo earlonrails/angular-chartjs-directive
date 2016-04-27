c892b986a7130693a6426c0264674a4d8100c5da
Kevin Krauss <earlkrauss@gmail.com>
2016-04-27 14:31:27 -0700

    some crazy invisible error

--- chartjs-directive.js
+++ chartjs-directive.js
@@ -1,55 +1,54 @@
 'use strict';
 
 angular.module('chartjs-directive', []).
-  directive('chart', function () {
-    var baseWidth = 600;
-    var baseHeight = 400;
+directive('chart', function() {
+  var baseWidth = 600;
+  var baseHeight = 400;
 
-    return {
-      restrict: 'E',
-      template: '<canvas></canvas>',
-      scope: {
-        chartObject: "=value"
-      },
-      link: function (scope, element, attrs) {
-        var canvas, context, chart, options;
-        var init = function(type){
-          element.empty();
-          element.append('<canvas></canvas>');
+  return {
+    restrict: 'E',
+    template: '<canvas></canvas>',
+    scope: {
+      chartObject: "=value"
+    },
+    link: function(scope, element, attrs) {
+      var canvas, context, chart, chartOptions = {};
 
-          canvas  = element.find("canvas")[0];
-          context = canvas.getContext("2d");
+      var updateChart = function() {
+        element.empty();
+        element.append('<canvas></canvas>');
 
-          options = {
-              type:   type   || "Line",
-              width:  attrs.width  || baseWidth,
-              height: attrs.height || baseHeight,
-              last_type: false
-          };
-          canvas.height = options.height;
-          canvas.width = options.width;
-          chart = new Chart(context);
+        canvas = element.find("canvas")[0];
+        context = canvas.getContext("2d");
+
+        var chartData = {
+          labels: [],
+          datasets: []
         };
-        init(attrs.type);
 
-        scope.$watch(function(){ return element.attr('type'); }, function(value){
-          if (!value) return;
-          init(value);
-          var chartType = options.type;
-          if(typeof scope.chartObject !== 'undefined'){
-            chart[chartType](scope.chartObject.data, scope.chartObject.options);
-          }
-          chart[chartType](scope.chartObject.data, scope.chartObject.options);
-        });
+        if (scope.chartObject) {
+          chartOptions = scope.chartObject;
+        } else {
+          chartOptions = {
+            type: "line",
+            options: {
+              width: attrs.width || baseWidth,
+              height: attrs.height || baseHeight,
+            },
+            data: chartData
+          };
+        }
 
-        //Update when charts data changes
-        scope.$watch(function() { return scope.chartObject; }, function(value) {
-          if (!value) return;
-          var chartType = options.type;
-          chart[chartType](scope.chartObject.data, scope.chartObject.options);
-          if (scope.chartInstance) scope.chartInstance.destroy();
-          scope.chartInstance = chart[chartType](scope.chartObject.data, scope.chartObject.options);
-        }, true);
-      }
+        new Chart(context, chartOptions);
+      };
+
+      //Update when charts data changes
+      scope.$watch(function() {
+        return scope.chartObject;
+      }, function(value) {
+        if (!value) return scope.chartObject;
+        updateChart();
+      });
     }
-  });
+  }
+});
